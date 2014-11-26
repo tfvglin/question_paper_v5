f12a9dbf91c8abf86fee88b4868294cff379614a
lsn <574823919@qq.com>
2014-11-26 22:18:05 +0800

    11.26 com

--- src/edu/xidian/research/util/CountUtil.java
+++ src/edu/xidian/research/util/CountUtil.java
@@ -1,5 +1,7 @@
 package edu.xidian.research.util;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 
@@ -21,13 +23,48 @@ public class CountUtil {
 	
 	public List<Integer> multipleAnswerOptionNum(List<String[]> maonlist,int questionoptionnum)
 	{
+		List<Integer> list =new ArrayList<Integer>();
 		int[] maon= new  int[questionoptionnum];
 		Iterator<String[]> it = maonlist.iterator();
 		while(it.hasNext())
 		{
 			String[] mulanswer = it.next();
+			for(int i=0;i<mulanswer.length;i++)
+			{
+				switch (mulanswer[i]) {
+				case "A":
+					maon[0]++;
+					break;
+				case "B":
+					maon[1]++;
+					break;
+				case "C":
+					maon[2]++;
+					break;
+				case "D":
+					maon[3]++;
+					break;
+				case "E":
+					maon[4]++;
+					break;
+				case "F":
+					maon[5]++;
+					break;
+				case "G":
+					maon[6]++;
+					break;
+					
+				default:
+					break;
+				}
+			}
 			
 		}
+		for(int num:maon)
+		{
+			list.add(num);
+		}
+		return list;
 	}
 
 }
